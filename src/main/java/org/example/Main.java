d696878452beb387ff31519d06b3bd1fdc6b44c8
haticerimmm023-png <haticerimmm023@gmail.com>
2026-02-19 12:29:17 +0300

    Sprint 13 Day 1 completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,31 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) return false;
+        if (!isBarking) return false;
+        return (clock >= 20 || clock < 8);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return (firstAge >= 13 && firstAge <= 19)
+                || (secondAge >= 13 && secondAge <= 19)
+                || (thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int upper = isSummer ? 45 : 35;
+        return temp >= 25 && temp <= upper;
     }
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) return -1;
+        return width * height;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) return -1;
+        return radius * radius * Math.PI;
     }
 }
